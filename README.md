bc21a7c6ef5a4d7235d4394e8f17778102a1afa9
ale-pil02 <57944020+ale-pil02@users.noreply.github.com>
2021-02-05 19:42:59 +0100

    MioProgetto

---
+++ README.md
@@ -0,0 +1,3 @@
+# progTecFinale
+
+Developed with Unreal Engine 4
---
+++ Source/progTecFinale/provaC.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "provaC.h"
+
+// Sets default values
+AprovaC::AprovaC()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AprovaC::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AprovaC::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
